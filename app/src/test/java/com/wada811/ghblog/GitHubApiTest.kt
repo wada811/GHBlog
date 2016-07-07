e5c59553eeda0c37e64eea940e2119b8c140f03c
wada811 <at.wada811@gmail.com>
2016-07-07 19:54:21 +0900

    :green_heart: Fix assert

--- app/src/test/java/com/wada811/ghblog/GitHubApiTest.kt
+++ app/src/test/java/com/wada811/ghblog/GitHubApiTest.kt
@@ -7,7 +7,7 @@ import com.wada811.ghblog.domain.model.GitCommit
 import com.wada811.ghblog.domain.model.GitHubTree
 import com.wada811.ghblog.domain.model.RepositoryContent
 import com.wada811.ghblog.domain.model.RepositoryContentInfo
-import junit.framework.Assert.assertEquals
+import junit.framework.Assert.*
 import org.junit.Before
 import org.junit.Test
 
@@ -22,11 +22,14 @@ class GitHubApiTest {
     @Test
     fun getRepositoryList() {
         GHBlogContext.userRepository.user().subscribe { user ->
-            user.repositoryList.subscribe({ repositoryList ->
-                repositoryList.forEach { repository ->
-                    assertEquals("repository", repository.toString())
-                }
-            }, { System.out.println("error: $it") }, {})
+            user.repositoryList.subscribe({
+                it.forEach { System.out.println("onNext: ${it.fullName}") }
+                assertNotNull(it)
+            }, {
+                System.out.println("error: $it")
+                it.printStackTrace()
+                assertNull(it)
+            })
         }
     }
 
@@ -38,14 +41,24 @@ class GitHubApiTest {
                     System.out.println("repository.name: ${repository.name}")
                     repository.name.equals("blogtest")
                 }
-                repository.getContents(user, "content/blog").subscribe({ repositoryContentInfos: List<RepositoryContentInfo> ->
-                    for (content in repositoryContentInfos) {
-                        System.out.println("content.name: ${content.name}")
-                    }
-                    assertEquals("onNext", "onNext")
-                }, { System.out.println("error: $it") }, {})
-            }, { System.out.println("error: $it") }, {})
-        }, { System.out.println("error: $it") }, {})
+                repository.getContents(user, "content/blog").subscribe({
+                    it.forEach { System.out.println("content.name: ${it.name}") }
+                    assertNotNull(it)
+                }, {
+                    System.out.println("error: $it")
+                    it.printStackTrace()
+                    assertNull(it)
+                })
+            }, {
+                System.out.println("error: $it")
+                it.printStackTrace()
+                assertNull(it)
+            })
+        }, {
+            System.out.println("error: $it")
+            it.printStackTrace()
+            assertNull(it)
+        })
     }
 
 
@@ -59,15 +72,31 @@ class GitHubApiTest {
                 }
                 repository.getContents(user, "content/blog").subscribe({ repositoryContentInfoList: List<RepositoryContentInfo> ->
                     repositoryContentInfoList.first { it.name.equals("test.md") }
-                            .getContent(user, repository)
-                            .subscribe({ content: RepositoryContent ->
-                                System.out.println("content.encoding: ${content.encoding}")
-                                System.out.println("content.content: ${content.encodedContent}")
-                                assertEquals("onNext", content.content)
-                            }, { System.out.println("error: $it") }, {})
-                }, { System.out.println("error: $it") }, {})
-            }, { System.out.println("error: $it") }, {})
-        }, { System.out.println("error: $it") }, {})
+                        .getContent(user, repository)
+                        .subscribe({ content: RepositoryContent ->
+                            System.out.println("content.encoding: ${content.encoding}")
+                            System.out.println("content.content: ${content.encodedContent}")
+                            assertEquals("onNext", content.content)
+                        }, {
+                            System.out.println("error: $it")
+                            it.printStackTrace()
+                            assertNull(it)
+                        })
+                }, {
+                    System.out.println("error: $it")
+                    it.printStackTrace()
+                    assertNull(it)
+                })
+            }, {
+                System.out.println("error: $it")
+                it.printStackTrace()
+                assertNull(it)
+            })
+        }, {
+            System.out.println("error: $it")
+            it.printStackTrace()
+            assertNull(it)
+        })
     }
 
     @Test
@@ -79,12 +108,24 @@ class GitHubApiTest {
                     repository.name.equals("blogtest")
                 }
                 repository.createContent(user, GitCommit("content/blog/test.md", "create test message", "create content body"))
-                        .subscribe({ gitHubCommit ->
-                            System.out.println("gitHubCommit: $gitHubCommit")
-                            assertEquals("onNext", gitHubCommit)
-                        }, { System.out.println("error: $it") }, {})
-            }, { System.out.println("error: $it") }, {})
-        }, { System.out.println("error: $it") }, {})
+                    .subscribe({ it ->
+                        System.out.println("onNext: $it")
+                        assertNotNull(it)
+                    }, {
+                        System.out.println("error: $it")
+                        it.printStackTrace()
+                        assertNull(it)
+                    })
+            }, {
+                System.out.println("error: $it")
+                it.printStackTrace()
+                assertNull(it)
+            })
+        }, {
+            System.out.println("error: $it")
+            it.printStackTrace()
+            assertNull(it)
+        })
     }
 
     @Test
@@ -98,13 +139,29 @@ class GitHubApiTest {
                 val path = "content/blog/test.md"
                 repository.getContent(user, path).subscribe({
                     repository.updateContent(user, GitCommit(path, "update test message", "update content body", it.sha))
-                            .subscribe({ gitHubCommit ->
-                                System.out.println("gitHubCommit: $gitHubCommit")
-                                assertEquals("onNext", gitHubCommit)
-                            }, { System.out.println("error: $it") }, {})
-                }, { System.out.println("error: $it") }, {})
-            }, { System.out.println("error: $it") }, {})
-        }, { System.out.println("error: $it") }, {})
+                        .subscribe({ gitHubCommit ->
+                            System.out.println("gitHubCommit: $gitHubCommit")
+                            assertEquals("onNext", gitHubCommit)
+                        }, {
+                            System.out.println("error: $it")
+                            it.printStackTrace()
+                            assertNull(it)
+                        })
+                }, {
+                    System.out.println("error: $it")
+                    it.printStackTrace()
+                    assertNull(it)
+                })
+            }, {
+                System.out.println("error: $it")
+                it.printStackTrace()
+                assertNull(it)
+            })
+        }, {
+            System.out.println("error: $it")
+            it.printStackTrace()
+            assertNull(it)
+        })
     }
 
     @Test
@@ -118,13 +175,29 @@ class GitHubApiTest {
                 val path = "content/blog/test.md"
                 repository.getContent(user, path).subscribe({
                     repository.deleteContent(user, it.createCommit("delete test message"))
-                            .subscribe({ gitHubCommit ->
-                                System.out.println("gitHubCommit: $gitHubCommit")
-                                assertEquals("onNext", gitHubCommit)
-                            }, { System.out.println("error: $it") }, {})
-                }, { System.out.println("error: $it") }, {})
-            }, { System.out.println("error: $it") }, {})
-        }, { System.out.println("error: $it") }, {})
+                        .subscribe({ gitHubCommit ->
+                            System.out.println("gitHubCommit: $gitHubCommit")
+                            assertEquals("onNext", gitHubCommit)
+                        }, {
+                            System.out.println("error: $it")
+                            it.printStackTrace()
+                            assertNull(it)
+                        })
+                }, {
+                    System.out.println("error: $it")
+                    it.printStackTrace()
+                    assertNull(it)
+                })
+            }, {
+                System.out.println("error: $it")
+                it.printStackTrace()
+                assertNull(it)
+            })
+        }, {
+            System.out.println("error: $it")
+            it.printStackTrace()
+            assertNull(it)
+        })
     }
 
     @Test
@@ -137,8 +210,20 @@ class GitHubApiTest {
                 }
                 repository.getTree(user).subscribe({ tree: GitHubTree ->
                     assertEquals("tree", tree.toString())
-                }, { System.out.println("error: $it") }, {})
-            }, { System.out.println("error: $it") }, {})
-        }, { System.out.println("error: $it") }, {})
+                }, {
+                    System.out.println("error: $it")
+                    it.printStackTrace()
+                    assertNull(it)
+                })
+            }, {
+                System.out.println("error: $it")
+                it.printStackTrace()
+                assertNull(it)
+            })
+        }, {
+            System.out.println("error: $it")
+            it.printStackTrace()
+            assertNull(it)
+        })
     }
 }
