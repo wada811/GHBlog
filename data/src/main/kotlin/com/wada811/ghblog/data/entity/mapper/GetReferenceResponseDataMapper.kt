acc46b4e01e91d8fde943e6d97718a3c2601846b
wada811 <at.wada811@gmail.com>
2016-07-11 10:48:06 +0900

    :recycle: Add GetReferenceResponseDataMapper and UpdateReferenceResponseDataMapper

---
+++ data/src/main/kotlin/com/wada811/ghblog/data/entity/mapper/GetReferenceResponseDataMapper.kt
@@ -0,0 +1,22 @@
+package com.wada811.ghblog.data.entity.mapper
+
+import com.wada811.ghblog.data.entity.response.github.git.refs.GetReferenceResponse
+import com.wada811.ghblog.data.entity.response.github.git.refs.GetReferenceResponse.GetReferenceObjectResponse
+import com.wada811.ghblog.domain.model.Reference
+import com.wada811.ghblog.domain.model.Reference.ReferenceObject
+
+object GetReferenceResponseDataMapper {
+    fun transform(response: GetReferenceResponse) = Reference(
+        response.ref,
+        response.url,
+        ReferenceObjectResponseDataMapper().transform(response.`object`)
+    )
+
+    class ReferenceObjectResponseDataMapper() {
+        fun transform(response: GetReferenceObjectResponse) = ReferenceObject(
+            response.type,
+            response.sha,
+            response.url
+        )
+    }
+}
---
+++ data/src/main/kotlin/com/wada811/ghblog/data/entity/mapper/UpdateReferenceResponseDataMapper.kt
@@ -0,0 +1,22 @@
+package com.wada811.ghblog.data.entity.mapper
+
+import com.wada811.ghblog.data.entity.response.github.git.refs.UpdateReferenceResponse
+import com.wada811.ghblog.data.entity.response.github.git.refs.UpdateReferenceResponse.UpdateReferenceObjectResponse
+import com.wada811.ghblog.domain.model.Reference
+import com.wada811.ghblog.domain.model.Reference.ReferenceObject
+
+object UpdateReferenceResponseDataMapper {
+    fun transform(response: UpdateReferenceResponse) = Reference(
+        response.ref,
+        response.url,
+        ReferenceObjectResponseDataMapper().transform(response.`object`)
+    )
+
+    class ReferenceObjectResponseDataMapper() {
+        fun transform(response: UpdateReferenceObjectResponse) = ReferenceObject(
+            response.type,
+            response.sha,
+            response.url
+        )
+    }
+}
