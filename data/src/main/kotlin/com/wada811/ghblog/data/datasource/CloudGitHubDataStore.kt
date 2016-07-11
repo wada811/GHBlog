e2c9abc4533e6ec4036e5d2e1da785d249ca94be
wada811 <at.wada811@gmail.com>
2016-07-11 09:19:40 +0900

    :recycle: Add CreateContentResponseDataMapper

--- data/src/main/kotlin/com/wada811/ghblog/data/datasource/CloudGitHubDataStore.kt
+++ data/src/main/kotlin/com/wada811/ghblog/data/datasource/CloudGitHubDataStore.kt
@@ -1,9 +1,6 @@
 package com.wada811.ghblog.data.datasource
 
-import com.wada811.ghblog.data.entity.mapper.GetContentResponseDataMapper
-import com.wada811.ghblog.data.entity.mapper.GetContentsResponseDataMapper
-import com.wada811.ghblog.data.entity.mapper.GitTreeEntityDataMapper
-import com.wada811.ghblog.data.entity.mapper.RepositoryResponseDataMapper
+import com.wada811.ghblog.data.entity.mapper.*
 import com.wada811.ghblog.data.entity.request.github.git.trees.CreateTreeRequest
 import com.wada811.ghblog.data.entity.request.github.git.trees.CreateTreeRequest.CreateTreeBodyRequest
 import com.wada811.ghblog.data.entity.request.github.git.trees.CreateTreeRequest.CreateTreeBodyRequest.CreateTreeTreeRequest
@@ -45,16 +42,14 @@ class CloudGitHubDataStore(var user: User) {
     fun getContents(repository: Repository, path: String): Observable<List<RepositoryContentInfo>> {
         return Observable.defer {
             val request = GetContentsRequest(repository.owner.login, repository.name, path)
-            GitHubApi(user).getContents(request)
-                .map { it.body().map { GetContentsResponseDataMapper.transform(it) } }
+            GitHubApi(user).getContents(request).map { it.body().map { GetContentsResponseDataMapper.transform(it) } }
         }
     }
 
     fun getContent(repository: Repository, path: String): Observable<RepositoryContent> {
         return Observable.defer {
             val request = GetContentRequest(repository.owner.login, repository.name, path)
-            GitHubApi(user).getContent(request)
-                .map { GetContentResponseDataMapper.transform(it.body()) }
+            GitHubApi(user).getContent(request).map { GetContentResponseDataMapper.transform(it.body()) }
         }
     }
 
@@ -63,55 +58,7 @@ class CloudGitHubDataStore(var user: User) {
             val request = CreateContentRequest(repository.owner.login, repository.name, commit.path,
                 CreateContentRequest.CreateContentCommitRequest(commit.path, commit.message, commit.encodedContent())
             )
-            GitHubApi(user).createContent(request)
-                .map {
-                    val response = it.body()
-                    GitHubCommit(
-                        RepositoryContentInfo(
-                            response.content.name,
-                            response.content.path,
-                            response.content.sha,
-                            response.content.size,
-                            response.content.url,
-                            response.content.html_url,
-                            response.content.git_url,
-                            response.content.download_url,
-                            response.content.type,
-                            RepositoryContentInfo.ContentLink(
-                                response.content._links.self,
-                                response.content._links.git,
-                                response.content._links.html
-                            )
-                        ),
-                        GitHubCommit.Commit(
-                            response.commit.sha,
-                            response.commit.url,
-                            response.commit.html_url,
-                            GitHubCommit.Commit.Author(
-                                response.commit.author.date,
-                                response.commit.author.name,
-                                response.commit.author.email
-                            ),
-                            GitHubCommit.Commit.Author(
-                                response.commit.committer.date,
-                                response.commit.committer.name,
-                                response.commit.committer.email
-                            ),
-                            response.commit.message,
-                            GitHubTree(
-                                response.commit.tree.sha,
-                                response.commit.tree.url
-                            ),
-                            response.commit.parents.map {
-                                GitHubReference(
-                                    it.sha,
-                                    it.url,
-                                    it.html_url
-                                )
-                            }
-                        )
-                    )
-                }
+            GitHubApi(user).createContent(request).map { CreateContentResponseDataMapper.transform(it.body()) }
         }
     }
 
