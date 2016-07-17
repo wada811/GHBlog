a901b8ce21f80b7d8f4fee9a129a29c79c5ee10e
wada811 <at.wada811@gmail.com>
2016-07-17 21:13:34 +0900

    :sparkles: Finish when article created

--- app/src/main/kotlin/com/wada811/ghblog/view/activity/ArticleCreateActivity.kt
+++ app/src/main/kotlin/com/wada811/ghblog/view/activity/ArticleCreateActivity.kt
@@ -7,7 +7,10 @@ import android.support.v7.app.AppCompatActivity
 import com.wada811.ghblog.R
 import com.wada811.ghblog.view.binding.ArticleCreateActivityBindingAdapter
 import com.wada811.ghblog.viewmodel.ArticleCreateViewModel
+import com.wada811.rxviewmodel.RxMessenger
+import rx.android.schedulers.AndroidSchedulers
 import rx.subscriptions.CompositeSubscription
+import java.util.concurrent.TimeUnit
 
 class ArticleCreateActivity : AppCompatActivity() {
 
@@ -21,11 +24,24 @@ class ArticleCreateActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ArticleCreateActivityBindingAdapter(this, R.layout.activity_article_create)
         binding.viewModel = ArticleCreateViewModel()
+        subscriptions.add(RxMessenger
+            .toObservable()
+            .ofType(SaveAction::class.java)
+            .throttleFirst(300, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                finish()
+            }
+        )
     }
 
     override fun onDestroy() {
         subscriptions.unsubscribe()
         binding.viewModel.unsubscribe()
         super.onDestroy()
+    }
+
+    class SaveAction {
+
     }
 }
--- app/src/main/kotlin/com/wada811/ghblog/viewmodel/ArticleCreateViewModel.kt
+++ app/src/main/kotlin/com/wada811/ghblog/viewmodel/ArticleCreateViewModel.kt
@@ -3,7 +3,9 @@ package com.wada811.ghblog.viewmodel
 import android.util.Log
 import android.view.View
 import com.wada811.ghblog.domain.GHBlogContext
+import com.wada811.ghblog.view.activity.ArticleCreateActivity
 import com.wada811.rxviewmodel.RxCommand
+import com.wada811.rxviewmodel.RxMessenger
 import com.wada811.rxviewmodel.RxProperty
 import com.wada811.rxviewmodel.RxViewModel
 import rx.android.schedulers.AndroidSchedulers
@@ -11,8 +13,8 @@ import rx.schedulers.Schedulers
 
 class ArticleCreateViewModel : RxViewModel() {
     var path = RxProperty("").asManaged()
-    var name = RxProperty("name").asManaged()
-    var content = RxProperty("content").asManaged()
+    var name = RxProperty("").asManaged()
+    var content = RxProperty("").asManaged()
     var save = RxCommand(View.OnClickListener {
         GHBlogContext.currentUser.currentRepository!!
             .createContent(path.value!!, "Create ${path.value}", name.value + System.getProperty("line.separator") + content.value)
@@ -20,6 +22,7 @@ class ArticleCreateViewModel : RxViewModel() {
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
                 Log.e("wada", "currentRepository.createContent.onNext")
+                RxMessenger.send(ArticleCreateActivity.SaveAction())
             }, {
                 Log.e("wada", "currentRepository.createContent.onError", it)
             }, {
