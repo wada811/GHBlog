8d197664329de8403969f27132c8e68ad040130e
wada811 <at.wada811@gmail.com>
2016-10-12 23:06:01 +0900

    :+1: Show article publish date

--- app/src/main/kotlin/com/wada811/ghblog/viewmodel/ArticleListItemViewModel.kt
+++ app/src/main/kotlin/com/wada811/ghblog/viewmodel/ArticleListItemViewModel.kt
@@ -1,11 +1,15 @@
 package com.wada811.ghblog.viewmodel
 
 import com.wada811.ghblog.domain.model.Article
+import com.wada811.rxviewmodel.RxProperty
 import com.wada811.rxviewmodel.RxViewModel
 import com.wada811.rxviewmodel.extensions.ObserveProperty
 import com.wada811.rxviewmodel.extensions.toRxProperty
+import org.threeten.bp.format.DateTimeFormatter
 
-class ArticleListItemViewModel(var article: Article) : RxViewModel() {
-    var articleTitle = article.ObserveProperty("title", { it.title }).toRxProperty(article.title).asManaged()
-    var articleBody = article.ObserveProperty("body", { it.body }).toRxProperty(article.body).asManaged()
+class ArticleListItemViewModel(val article: Article) : RxViewModel() {
+    val articleTitle = article.ObserveProperty("title", { it.title }).toRxProperty(article.title).asManaged()
+    val articlePublishDate: RxProperty<String>
+        = article.ObserveProperty("publishDateTime", { it.publishDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE) })
+        .toRxProperty(article.publishDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE)).asManaged()
 }
